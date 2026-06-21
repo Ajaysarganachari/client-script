7574d5298e0b6d1665a591b093cccb752f5ba1e0
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2026-06-21 15:48:23 +0530

    Update work on caller field.js

--- glide user./work on caller field.js
+++ glide user./work on caller field.js
@@ -20,3 +20,31 @@ function onChange(control, oldValue, newValue, isLoading, isTemplate) {
    //Type appropriate comment here, and begin script below
    }
 }
+
+
+example 2=
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+   if(g_form.isNewRecord()){
+
+   
+   
+   if (isLoading || newValue === '') {
+
+	g_form.setValue("description",g_user.getFullName());
+	g_form.setReadOnly("description",true);
+      return;
+   }
+
+   if(newValue == "true"){
+    g_form.clearMessages("description");
+	g_form.setReadOnly("description",false);
+	 g_form.setValue("description",g_user.firstName);
+	 g_form.setLabelOf("description","New User");
+   }else{
+		g_form.setValue("description",g_user.getFullName());
+	g_form.setReadOnly("description",true);
+   }
+
+   //Type appropriate comment here, and begin script below
+   }
+}
